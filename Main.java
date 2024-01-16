e39834aa03694389f4b74ff7ecb5285d850258e7
Skyluc05 <152433467+Skyluc05@users.noreply.github.com>
2024-01-16 14:58:25 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.security.PublicKey;
+
+public class Main {
+    public static void FeldErstellen(){
+        char [][] Feld = new char[5][5];
+    }
+    public static void Feldbefuellen(){
+        for (int i = 0;i <= 5;i++){
+            for(int j = 0;j <= 5;j++){
+
+            }
+        }
+        int x = (int)(Math.random() * 5 + 0);
+        int y = (int)(Math.random() * 5 + 0);
+    }
+    public static void main(String[] args)
+    {
+        FeldErstellen();
+        Feldbefuellen();
+        }
+    }
